00c1033f0f8e648d8f447c470d78ccbbf3b63355
shew0015 <111554720+shew0015@users.noreply.github.com>
2022-10-08 22:14:33 +1100

    Add files via upload

---
+++ datavisualisation2.js
@@ -0,0 +1,9 @@
+var vg_6 = "melbournemaxtempdate.json";
+vegaEmbed("#vis6", vg_6, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_7 = "melbournemaxtempyear.json";
+vegaEmbed("#vis7", vg_7, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
